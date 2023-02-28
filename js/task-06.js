5e03d80c4b7d7971cf6eeaed1d35bd6ab59c5347
Yan Razvadovskyi <116461898+yanrzv@users.noreply.github.com>
2023-03-01 00:21:19 +0200

    06 update

--- js/task-06.js
+++ js/task-06.js
@@ -4,9 +4,11 @@ const INPUT_DATA_LENGTH = inputArea.dataset.length;
 
 inputArea.addEventListener('blur', (event) => {
     if (event.currentTarget.value.length < INPUT_DATA_LENGTH) {
-        inputArea.classList.add('invalid')
+        inputArea.classList.add('invalid');
+        inputArea.classList.remove('valid');
+    } else {
+        inputArea.classList.add('valid');
+        inputArea.classList.remove('invalid');
     }
 
-    inputArea.classList.add('valid');
-   
 });
