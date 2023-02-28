a7c3b816e4221938c07c553ecd8f07e33986915c
Yan Razvadovskyi <116461898+yanrzv@users.noreply.github.com>
2023-03-01 00:15:58 +0200

    9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+    if(email.value === '' || password.value === '') {
+        alert('Всі поля повинні бути заповнені')
+    } else {
+        console.log(`{ email: ${email.value}, password: ${password.value} }`);
+    }
+
+    event.currentTarget.reset();
+});
+
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const bgColorValue = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  bgColorValue.textContent = body.style.backgroundColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
